91d951f220d7ab9d8fb25951c5b0cd7e4d68c14b
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-05-20 06:43:44 -0500

    Cuarta  prueba

--- PruebasGithub/src/main/java/Pruebas.java
+++ PruebasGithub/src/main/java/Pruebas.java
@@ -2,4 +2,5 @@ public class Pruebas {
     private String prueba = "Esto es una prueba";
     private String pruebaDos = "Esto es una segunda prueba";
     private String pruebaTres = "Esto es una tercera prueba";
+    private String pruebaCuatro = "Esto es una cuarta prueba";
 }
